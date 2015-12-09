5bf63495a60ca54031fae6a133252d3f9b402f8e
nruhee <n.ruhee@advertstar.net>
2015-12-09 17:22:23 +0300

    ammendments

--- src/test/java/com/advertstar/net/WebmasterTraffic.java
+++ src/test/java/com/advertstar/net/WebmasterTraffic.java
@@ -54,7 +54,7 @@ public class WebmasterTraffic {
     public void setUp() throws Exception {
         driver = new FirefoxDriver();
         driver.manage().window().setSize(new Dimension(1920,1080));
-        driver.manage().window().maximize();
+
         baseUrl = "https://advertstar.net/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
